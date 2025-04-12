96fd7da5f11028c637c5d830958cc44b7ef6c11a
Arthur <baginian.artur@yandex.ru>
2025-04-12 18:03:21 +0300

    Create app wrapper class

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Self
 
 from dishka import make_container
 from dishka.integrations.flask import FlaskProvider, setup_dishka
@@ -6,30 +7,42 @@ from flask import Flask
 from flask_login import LoginManager
 from sqlalchemy.orm import Session
 
+from app.core.config import AppConfig
 from app.core.dependencies.flask import provider
 from app.lib.models.user import UserModel
 from app.routers.router import router
 
 
-def app() -> Flask:
-    base_dir = Path(__file__).resolve().parent.parent
-    template_dir = base_dir / "app" / "templates"
+BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = BASE_DIR / "app" / "templates"
 
-    app = Flask(__name__, template_folder=template_dir)
-    app.register_blueprint(router)
-    # TODO: move to envs
-    app.secret_key = "lol"
 
-    container = make_container(provider, FlaskProvider())
+class App:
+    def __init__(self, config: AppConfig, app: Flask | None = None) -> None:
+        self.config = config
+        self.app = app or Flask(__name__, template_folder=TEMPLATE_DIR)
+        self.login_manager = LoginManager()
 
-    def load_user(user_id: int):
-        with container() as request_container:
+        self.setup_app()
+
+    @classmethod
+    def from_env(cls) -> Self:
+        return cls(AppConfig.from_env())
+
+    def setup_app(self) -> None:
+        self.app.register_blueprint(router)
+        self.app.secret_key = self.config.security.token
+        self.container = make_container(provider, FlaskProvider())
+
+        self.login_manager.user_loader(self.load_user)
+        self.login_manager.init_app(app)
+
+        setup_dishka(container=self.container, app=self.app, auto_inject=True)
+
+    def load_user(self, user_id: int):
+        with self.container() as request_container:
             return request_container.get(Session).query(UserModel).get(user_id)
 
-    login_manager = LoginManager()
-    login_manager.user_loader(load_user)
-    login_manager.init_app(app)
 
-    setup_dishka(container=container, app=app, auto_inject=True)
-
-    return app
+def app() -> Flask:
+    return App.from_env().app
